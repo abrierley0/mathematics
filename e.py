90ab2e2599a1079ebdd95e018ef94c1cf27becbf
abrierley0 <brierley.ajb@gmail.com>
2025-07-15 12:12:46 +0100

    Added y = e^x graph

--- e.py
+++ e.py
@@ -10,22 +10,33 @@ yd = 0.693*(2**x)
 y1 = 3**x
 y1d = 1.099*(3**x)
 
-plt.figure(figsize=(10, 4))
+y2 = 2.71828**x
+y2d = y2
+
+plt.figure(figsize=(15, 4))
 
 # Subplot #1
-plt.subplot(1,2,1)
-plt.plot(x,y,'-kx', label='y = 2**x')
+plt.subplot(1,3,1)
+plt.plot(x,y,'-ko', label='$y = 2^x$')
 plt.plot(x,yd,'-rx', label='dy/dx')
 #plt.title('y = 2**x')
 plt.legend()
 plt.grid(True)
 
 # Subplot #2
-plt.subplot(1,2,2)
-plt.plot(x,y1,'-kx', label='y = 3**x')
+plt.subplot(1,3,2)
+plt.plot(x,y1,'-ko', label='$y = 3^x$')
 plt.plot(x,y1d,'-rx', label='dy/dx')
 plt.grid(True)
 #plt.title('y = 3**x')
+plt.legend()
+
+# Subplot #3
+plt.subplot(1,3,3)
+plt.plot(x,y2,'-ko', label='$y = 2.718^x = e^x$')
+plt.plot(x,y2d,'-rx', label='dy/dx')
+plt.grid(True)
+#plt.title('y = e**x')
 plt.legend()
 
 plt.suptitle('Exponential Functions and e = 2.71828(...)')
